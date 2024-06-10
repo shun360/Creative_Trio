424cdd10a7df0d58da0979d5c8b0bdf0eb88f7cf
shun360 <167048515+shun360@users.noreply.github.com>
2024-06-10 09:16:33 +0900

    ikou

--- Assets/SubCamera/SubCameraScript.cs
+++ Assets/SubCamera/SubCameraScript.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class SubCameraScript : MonoBehaviour
 {
-    private GameObject bollObj;
+    private GameObject ballObj;
     private Vector3 startPosi = new Vector3(0, 2, -8);
     // Start is called before the first frame update
     void Start()
     {
-        this.bollObj = GameObject.Find("Boll");
-        transform.position = bollObj.transform.position + startPosi;
-        transform.LookAt(bollObj.transform.position);
+        this.ballObj = GameObject.Find("Ball");
+        transform.position = ballObj.transform.position + startPosi;
+        transform.LookAt(ballObj.transform.position);
     }
 
     // Update is called once per frame
--- Assets/player/HeroScript.cs
+++ Assets/player/HeroScript.cs
@@ -14,21 +14,21 @@ public class NewBehaviourScript : MonoBehaviour
 {
     private Vector2 currentPosi;
     [SerializeField] private Transform moveTarget;
-    [SerializeField] private float smoothTime = 1f;
+    [SerializeField] private float smoothTime = 10f;
     private Vector2 currentSpeed;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
-        transform.position = new Vector2(50, 100);
-        //currentPosi = new Vector2(transform.position.x, transform.position.y);
-        /*transform.position = Vector2.SmoothDamp(
+        transform.position = new Vector2(0, 0);
+        currentPosi = new Vector2(transform.position.x, transform.position.y);
+        transform.position = Vector2.SmoothDamp(
             currentPosi,
             new Vector2(200, 200),
             ref currentSpeed,
             smoothTime
-        //途中　canvasの座標ではなく全体の座標に行ってしまう
-        );*/
+        
+        );
     }
 
 
