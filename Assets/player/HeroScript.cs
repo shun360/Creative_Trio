e9f3f9000e788664a5e9b779710daa9570c01964
shun360 <167048515+shun360@users.noreply.github.com>
2024-06-10 22:19:12 +0900

    hero variable

--- Assets/player/HeroScript.cs
+++ Assets/player/HeroScript.cs
@@ -16,15 +16,31 @@ public class NewBehaviourScript : MonoBehaviour
     private int heroATK = 8;
     private int heroDEF = 5;
     private int heroMagiATK = 15;
+    private int heroBlock = 0;
+
     private Vector2 currentPosi;
     [SerializeField] private Transform moveTarget;
     [SerializeField] private float smoothTime;
     private Vector2 currentSpeed;
     
+    //getter, setter
     public int getHeroHP() {  return heroHP; }
     public int getHeroATK() {  return heroATK; }
+    public int getHeroDEF() {  return heroDEF; }
+    public int getHeroMagiATK() { return heroMagiATK; }
+    public int getHeroBlock() {  return heroBlock; }
     public void setHeroHP(int hp) {  heroHP = hp; }
     public void setHeroATK(int atk) {  heroATK = atk; }
+    public void setHeroDEF(int def) {  heroDEF = def; }
+    public void setHeroMagiATK(int magiATK) { heroMagiATK= magiATK; }
+    public void setHeroBlock(int block) {  heroBlock = block; }
+    //other method
+    public void addHeroBlock() { heroBlock += heroDEF; }//ブロック値をプラスする
+    public void beAttacked(int damage) { 
+
+        //heroHP -= (damage - ); 
+    }//攻撃を受ける
+
     // Start is called before the first frame update
     void Start()
     {
