a03624b04e86b8d078e8ed74a3c036b30e322e2c
shun360 <167048515+shun360@users.noreply.github.com>
2024-08-25 15:57:42 +0900

    ビルド調整

--- Assets/pin/PinClass.cs
+++ Assets/pin/PinClass.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CommandType;
-using UnityEditor.Search;
 
 public class PinClass : MonoBehaviour
 {
--- Assets/tester/TesterScript.cs
+++ Assets/tester/TesterScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CommandType;
@@ -176,3 +176,4 @@ public class TesterScript : MonoBehaviour
         }
     }
 }
+*/
